670ec596d980845275f8409881ad495ad88f2703
Sayam753 <sayamkumar753@yahoo.in>
2020-03-26 12:19:26 +0530

    Added scheduler function

--- flask_ami.py
+++ flask_ami.py
@@ -1,3 +1,6 @@
+import time, atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from flask import Flask, url_for, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -73,5 +76,16 @@ def delete_route(route_id):
     return redirect(url_for('home'))
 
 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=5)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
