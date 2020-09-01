2ff027f37f384472274faa8cd88ef7c8de677c69
Sayam753 <sayamkumar753@yahoo.in>
2020-09-01 20:43:29 +0530

    Added form and rendering stuff

--- flask_ami.py
+++ flask_ami.py
@@ -1,13 +1,16 @@
-import time, atexit
+import time
+import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from flask import Flask, url_for, render_template, redirect, request
+from flask import Flask, url_for, render_template, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 # For forms
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from flask_login import UserMixin, LoginManager, login_user, current_user, login_required, logout_user
+from flask_bcrypt import Bcrypt
 
 
 # Initialize the flask application
@@ -15,6 +18,104 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '99f625427e95c4dbafe99f1d6e73abfd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User(email: {self.email})"
+
+
+class UserLoginForm(FlaskForm):
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                'That username is taken. Please choose a different one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a different one')
+
+
+# Reload the user from its session
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        flash(f"You are already logged in!", "warning")
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data,
+                    password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"You account has been created! You are now able to log in", "success")
+        return redirect(url_for('login'))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash(f"You are already logged in!", "warning")
+        return redirect(url_for('home'))
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        flash(f"Login unsuccessful! Please check email and password!", "danger")
+    return render_template("login.html", form=form)
+
+
+@app.route("/list-users")
+@login_required
+def list_users():
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 class Route(db.Model):
@@ -41,10 +142,12 @@ def home():
 
 
 @app.route('/route/new', methods=['GET', 'POST'])
+@login_required
 def new_route():
     form = RouteForm()
     if form.validate_on_submit():
-        route = Route(storeID=form.storeID.data, nexmoDID=form.nexmoDID.data, apiServer=form.apiServer.data)
+        route = Route(storeID=form.storeID.data,
+                      nexmoDID=form.nexmoDID.data, apiServer=form.apiServer.data)
         db.session.add(route)
         db.session.commit()
         return redirect(url_for('home'))
@@ -52,6 +155,7 @@ def new_route():
 
 
 @app.route('/route/<int:route_id>/update', methods=['GET', 'POST'])
+@login_required
 def update_route(route_id):
     route = Route.query.get(route_id)
     form = RouteForm()
@@ -69,6 +173,7 @@ def update_route(route_id):
 
 
 @app.route('/route/<int:route_id>/delete', methods=['POST'])
+@login_required
 def delete_route(route_id):
     route = Route.query.get(route_id)
     db.session.delete(route)
@@ -80,12 +185,12 @@ def print_date_time():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=5)
-scheduler.start()
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=print_date_time, trigger="interval", seconds=5)
+# scheduler.start()
 
 # Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
+# atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
